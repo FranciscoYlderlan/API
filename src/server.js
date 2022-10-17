78d4270b23f8fc3c16938fccb0d08cac5ce0432a
FranciscoYlderlan <ylderlan@nca.ufma.br>
2022-10-17 16:00:54 -0300

    Created post method

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ import  express  from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
 });
 
-app.get("/usuario/:id", (request, response) => {
+app.get("/users/:id", (request, response) => {
     const {id} = request.params;
     response.send(`Usuário cadastrado de id ${id}`);
 });
@@ -16,4 +18,10 @@ app.get("/usuario/:id", (request, response) => {
 app.get("/filme", (request, response) => {
     const {titulo, ator} = request.query;
     response.send(`O filme escolhido foi ${titulo} estrelado por ${ator}`);
+});
+
+
+app.post("/users/create", (request, response) => {
+    
+    response.send(request.body);
 });
