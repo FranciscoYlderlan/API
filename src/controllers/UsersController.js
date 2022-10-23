eb0c72171ecd565695f1e7f94f2aa18eaaffd501
FranciscoYlderlan <ylderlan@nca.ufma.br>
2022-10-23 14:47:20 -0300

    Add bcrypt module and hashed password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,17 +1,22 @@
 import knex from "../database/knex/index.js"
+import {hash, compare} from "bcrypt"
 
 export default class UsersController {
 
     async create(request, response) {
         const {id, name, email, password, avatar} = request.body;
-        const id_notes = await knex('users').insert({
+        
+        const encrypted_password = await hash(password,8);
+        
+        const id_user = await knex('users').insert({
             name,
             email,
-            password,
+            password: encrypted_password,
             avatar,
             updated_at: knex.fn.now(),
             created_at: knex.fn.now()
-        })
+        }).catch(error => console.error(error))
+
         response.status(201).json({})
     }
 }
