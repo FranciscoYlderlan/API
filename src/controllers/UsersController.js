c7057260d847b60be8512e9e0a73fb9b90d8ab86
FranciscoYlderlan <ylderlan@nca.ufma.br>
2022-10-24 12:01:56 -0300

    Add async-errors to monitor origin errors and create some validates errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,42 @@
 import knex from "../database/knex/index.js"
 import {hash, compare} from "bcrypt"
+import AppError from "../utils/AppError.js";
+
 
 export default class UsersController {
 
+    async index(request, response) {
+        const {id, name, email, password, avatar} = request.body;
+        
+     
+        response.status(200).json({})
+    }
+
+    async show(request, response) {
+        const {id, name, email, password, avatar} = request.body;
+        
+     
+        response.status(200).json({})
+    }
     async create(request, response) {
         const {id, name, email, password, avatar} = request.body;
         
+        const users = () => knex('Users');
+        
         const encrypted_password = await hash(password,8);
         
-        const id_user = await knex('users').insert({
+        const existsName = await users().where({ name }).first();
+        
+        const existsEmail = await users().where({ email }).first();
+
+        if(existsName) {
+            throw new AppError("Nome de usuário em uso. Por favor informe novo usuário.")
+        }
+        if(existsEmail) {
+            throw new AppError("Email de usuário em uso. Por favor informe novo email.")
+        }
+
+        const id_user = await users().insert({
             name,
             email,
             password: encrypted_password,
@@ -18,6 +46,19 @@ export default class UsersController {
         }).catch(error => console.error(error))
 
         response.status(201).json({})
+    }
+    async update(request, response) {
+        const {id, name, email, password, avatar} = request.body;
+        
+     
+        response.status(200).json({})
+    }
+
+    async delete(request, response) {
+        const {id, name, email, password, avatar} = request.body;
+        
+     
+        response.status(200).json({})
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import  express  from "express";
+import 'express-async-errors';
 import sqliteConnection from "./database/sqlite/index.js"; 
-import routes from "./routes/index.js"
-
+import routes from "./routes/index.js";
+import AppError from "./utils/AppError.js";
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 sqliteConnection();
+
+app.use((error,request,response,next) => {
+    if (error instanceof AppError) 
+        return response.status(error.statusCode)
+                       .json(error.message());
+
+    console.error(error)
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = 8080;
 
