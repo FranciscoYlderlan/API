64b488fef1c2d71a8413776084730306a98f78be
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-02-20 10:14:19 -0300

    Added handler to save and delete files

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,20 @@
+import uploadsConfig from "./../configs/uploads.js";
+import path from 'path';
+import fs from 'fs'
+export class DiskStorage {
+    async saveFile(filename){
+        await fs.promises.rename(
+            path.resolve(uploadsConfig.TMP_FOLDER, filename),
+            path.resolve(uploadsConfig.UPLOADS_FOLDER, filename)
+        )
+    }
+    async deleteFile(filename){
+        const filepath = path.resolve(uploadsConfig.TMP_FOLDER, filename);
+        try {
+          await fs.promises.stat(filepath);  
+        } catch (error) {
+            console.log("Falha ao deletar arquivo: ", error);
+        }
+        await fs.promises.unlink(filepath);
+    }
+}
