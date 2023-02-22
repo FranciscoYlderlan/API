fbbcf2aa973f3607c9eb6dbe5f371976363a84ec
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-02-22 09:53:20 -0300

    fix: Saving  update method from AvatarUserController

--- src/controllers/AvatarUserController.js
+++ src/controllers/AvatarUserController.js
@@ -2,26 +2,26 @@ import knex from "../database/knex/index.js";
 import AppError from "../utils/AppError.js";
 import { DiskStorage } from "../providers/DiskStorage.js";
 
-
-const diskStorage = new DiskStorage();
-
 export default class AvatarUserController {
     async update(request, response) {
         const id = request.user.id;
-        
+        const filename = request.file.filename;
+
         const Tusers = () => knex("Users");
         const user = await Tusers().where({id}).first();
         
-        if(!user) throw new AppError('Usuário não cadastrado.');
+        const diskStorage = new DiskStorage();
+        
+        if(!user) throw new AppError('Usuário não autenticado.',401);
 
         if(user.avatar) await diskStorage.deleteFile(user.avatar);
 
         await diskStorage.saveFile(user.avatar);
-        
-        await Tusers().where({id}).update({
-            avatar: user.avatar
-        }).catch(error => console.error(error));
 
-        response.status(200).json({})
+        user.avatar = filename;
+        
+        await Tusers().where({id}).update(user).catch(error => console.error(error));
+
+        response.status(200).json({user})
     }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express"
 import UsersController from "../controllers/UsersController.js";
+import AvatarUserController from "../controllers/AvatarUserController.js";
 import ensureAuthentication from "../middlewares/ensureAuthentication.js";
-
+import multer from "multer";
 const usersRoutes = Router();
 const usersController = new UsersController();
+
+const avatarUserController = new AvatarUserController();
 
 // usersRoutes.get("/", usersController.index);
 // usersRoutes.get("/:id", usersController.show);
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/",ensureAuthentication,usersController.update);
+usersRoutes.put("/avatar",ensureAuthentication,uploads.single("avatar"),avatarUsersController.update);
 // usersRoutes.delete("/:id", usersController.delete);
 
 export default usersRoutes;
